325fd77c53f8da80871e1f2ff80a71c9b5ab6c44
samueldahl <samjoedahl@gmail.clom>
2017-11-30 09:05:50 -0700

    added a function that tests if you are Jared.

---
+++ morescript/morescript.js
@@ -0,0 +1,8 @@
+export function ismynamejared(){
+    var name = prompt("What is your name?");
+    if (name = "jared"){
+        console.log("yes, your name is jared");
+    }else{
+        console.log("sorry, you loose.");
+    }
+}
---
+++ script.js
@@ -0,0 +1,2 @@
+import{}from './morescript/evenmorescript.js';
+import{ismynamejared}from './morescript/morescript.js';
